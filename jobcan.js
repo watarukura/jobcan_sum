c0c4c2c0425469d4656738766450b02e870e7814
Wataru Kurashima <what.r.j150@gmail.com>
2021-12-04 09:27:31 +0900

    feat: 🎸 "状態": "有" is half-holiday

--- jobcan.js
+++ jobcan.js
@@ -1,14 +1,22 @@
 javascript:(function () {
-    if (location.href != "https://ssl.jobcan.jp/m/work/conditions") {
+    if (!location.href.startsWith("https://ssl.jobcan.jp/m/work/conditions")) {
         alert("failed");
         return;
     }
+    const yesterday = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleDateString();
+    const page_month = document.querySelector("#container > table.data03 > tbody > tr > th").innerText.replace(/[年月]/, '/');
     const schedules = Array.from(document.querySelector(".schedule4").rows);
     const sum = schedules
-        .slice(0, -1)
-        .map(schedule => schedule.children[5].innerText)
-        .filter(time => time.includes("時間"))
-        .map(p => parseInt(p.substr(0,2)) *60 + parseInt(p.substr(4,2)) -480)
+        .map(function(schedule) {
+           return {
+             date: schedule.children[0].innerText,
+             state: schedule.children[1].innerText,
+             time: schedule.children[5].innerText
+           };
+         })
+        .filter(schedule => schedule.time.includes("時間"))
+        .filter(schedule => page_month.concat(schedule.date) != yesterday)
+        .map(p => (p.state == "有" ? -240 : -480) + parseInt(p.time.substr(0,2)) *60 + parseInt(p.time.substr(4,2)))
         .reduce((prev, curr) => prev + curr);
     alert(`${sum}分の差異があります`);
 })()
